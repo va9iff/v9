eee78664d66a83f43a14b39310de6ea860b86663
va9iff <va9iff@gmail.com>
2023-05-11 18:19:40 +0400

    add cool text mouse press hover

--- src/draft/coolText/coolText.js
+++ src/draft/coolText/coolText.js
@@ -1,25 +1,24 @@
 import { html, VLitElement } from "/src/vlit.js"
 
 function coolify(str, opts) {
-	return str
-		.split("")
-		.map(
-			s =>
-				`<span class="coolText ${opts?.extra ? "extra" : ""} ${opts?.class || ""} ${
-					s == " " ? "white" : ""
-				}" 
-				>${s}</span>`
-		)
-		.join("")
+	let classes = []
+	if (opts?.extra) classes.push("extra")
+	if (opts?.class) classes.push(opts.class)
+	return (
+		'<span class="coolText">' +
+		str.split("").map(
+				s => `<span${classes.length ? ` class = ${classes.join(" ")}` : ""}>${s}</span>${s == " " ? " " : s == "\n" ? "<br>":""}`
+			)
+			.join("")+
+		"</span>"
+	)
 }
 
-document.body.innerHTML =
-	coolify("Social Platforms") +
-	"<br>" +
-	coolify("and Network ") +
-	coolify("Epmhesis", { extra: true/*, class: 'fasad'*/}) +
-	"<br>" +
-	coolify("and Continue") +
-	"dada"
-
+document.body.innerHTML =	coolify(`
+	Social Platforms
+	and Network
+	`) + coolify("Epmhesis", { extra: true /*, class: 'fasad'*/ }) +
+		"<br>" +
+		coolify("and Continue") +
+		"dada"
 // custom color with {class: fasad} and .fasad { -- --highlight: #22f3; } in css
