3484446745fc81b5720c416f7cd44c6af6c58a20
va9iff <va9iff@gmail.com>
2023-05-11 20:18:20 +0400

    just a few notes

--- src/vlit.js
+++ src/vlit.js
@@ -9,9 +9,20 @@ import {
 class VLitElement extends LitElement {
 	static props = {} // reactive property: default value
 	static properties = {} // reactive property: options
+	// static shadow = false
 	createRenderRoot() {
+		// if (!this.constructor.shadow) return this // disable shadow root
+		// return super.createRenderRoot()
 		return this // disable shadow root
 	}
+
+	// not implemented
+	// static shadow = true // needed for static styles property
+	// static styles = css`
+	// button{
+		// background: red}`
+
+
 	constructor() {
 		super()
 		for (let prop in this.constructor.props)
@@ -19,6 +30,7 @@ class VLitElement extends LitElement {
 		// assign every default value of property in the props to the instance
 	}
 	static set tag(tagName) {
+		// console.log(this.meta) // the url of the class which was imported
 		for (const prop in this.props) this.properties[prop] ??= {} // add reactiveness for the property
 		customElements.define(tagName, this) // assign custom element to a tag
 	}
