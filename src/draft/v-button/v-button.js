fd35ae1666ffc7c04820cf76b7b794f132ca1506
va9iff <va9iff@gmail.com>
2023-05-15 03:03:32 +0400

    add v-button

---
+++ src/draft/v-button/v-button.js
@@ -0,0 +1,36 @@
+import { html, VLitElement, styleMap } from "/src/vlit.js"
+
+class VButton extends VLitElement {
+	static properties = {
+		x: {},
+		y: {},
+	}
+	constructor() {
+		super()
+		this.x = 0
+		this.y = 0
+		this.text ??= this.getAttribute("text")
+		this.addEventListener("mousemove", this.hoverPositionFix)
+	}
+	hoverPositionFix = e => {
+		this.x = e.offsetX
+		this.y = e.offsetY
+	}
+	handler(e) {
+		console.log("clicked", this)
+	}
+	render() {
+		return html`
+			<button @click=${this.handler}>${this.text || this.button()}</button>
+			<div
+				class="circle"
+				style=${styleMap({
+					left: this.x + "px",
+					top: this.y + "px",
+				})}
+			></div>
+		`
+	}
+}
+
+VButton.tag = "v-button"
