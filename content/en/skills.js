3fa7d9879ca8b7a8f304affde8312811039ed40b
va9iff <va9iff@gmail.com>
2023-05-17 00:57:27 +0400

    adding more stuff

---
+++ content/en/skills.js
@@ -0,0 +1,53 @@
+export default {
+	JavaScript: {
+		explanation: `
+			I started to learn JavaScript after Python. At this point I was 
+			familiar with programming. Already learned HTML and css when i was 
+			a kid. I just had to learn "how"s. So I made a curriculum for 
+			learning how can I use JavaScript like Python:
+				<ol>
+					<li>How can I update</li>
+					<li>
+						How can I programmatically create <b>(XOU project)</b>
+					</li>
+				</ol>
+			This was all I need indeed. Took only 1 project to get comfortable 
+			with JS in less than a week of summer. Now I'm here :)
+		`
+	},
+	TypeScript: {
+		explanation: `Not too much tbh. Nice tooling tho.`
+	},
+	HTML: {
+		explanation: `I learned HTML in my 8th grade along with CSS. Couldn't 
+		accomplish much, created some blogs about my favorite games.`
+	},
+	CSS: {},
+	SASS: {},
+	Python: {
+		explanation: `Python was my <i>real</i> experience of programming. I 
+		made lots of games. My first real project was a card game from cli. The 
+		game was build around the order of cards. Player selects one card each 
+		turn. For example Fireball selected near Snow will "melt" snow cards. 
+		Cards go down when the card below them is removed. Some cards could 
+		be used at the end of the queue. I was realy happy with little CLI game 
+		that I learned Tkinter. But this wasn't fun. Ended up looking like a HR 
+		management app. So switched to Kivy. I could animate some properties 
+		here. But that was the time when I realized Python isn't the greatest 
+		choice for me... (see JS)`
+	},
+	GitHub: {},
+	React: {
+		explanation: `I usally don't use React for my projects. Lit is what I 
+		consider in general as it's more performant. Lit also makes use
+		of native custom element API instead of an abstraction. This website 
+		is built with Lit with every component. <br>
+		<br>
+		But I learned React and used it for small projects. Using it isn't hard 
+		for me at all. Lit is even harder to be honest with all these 
+		directives :D So I'm putting it here. No React exclusive projects, but 
+		I'm familiar with it.`
+	},
+	Firebase: {}
+
+}
