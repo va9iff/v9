3106354dc7589560f49738876d1350e421c4fdbe
va9iff <va9iff@gmail.com>
2023-05-22 03:58:50 +0400

    start to make network view and view styles

--- src/views/network/network.js
+++ src/views/network/network.js
@@ -7,9 +7,36 @@ class VNetwork extends View {
 	static properties = {
 	}
 	render() {
+		// maybe:
+		// content { padding: 42px }
+		// [indent] {margin-left: attr(indent)}
+		// or maybe even *[i] {margin-left: calc(attr(i) * 40px)}
+		/*
+			[i=1] is same as [indent]
+
+			content
+				h1[i=i]
+				div[i=1]
+					table
+
+
+		*/
+		// 
 		return html`
-			${Math.random()}
-			this is from network component
+			<div class="content">
+				<h1 indented>Social Media and Network</h1>
+				<div indented>
+					<h4 indented>Placces where you can reach me</h4>
+						<table indented class="socials">
+							<tr>
+								<td class="icon">f</td>
+								<td class="platform">Facebook</td>
+								<td class="username">@va9iff</td>
+								<td class="note">My elders forced me to</td>
+							</tr>
+						</table>
+				</div>
+			</div>
 		`
 	}
 }
