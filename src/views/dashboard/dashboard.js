8626ce0a109bc9f85fb0cfb2d30987b3e43e2e46
va9iff <va9iff@gmail.com>
2023-05-27 02:33:41 +0400

    add sticky and scrolls

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -6,18 +6,73 @@ await new Promise(r=>setTimeout(r,500))
 class VDashboard extends View {
 	static properties = {
 	}
+	scroll(e){
+		// console.log(this.scrollTop)
+		// this.classList.toggle("red", Math.random() > 0.05) // fun
+		this.classList.toggle("scrolled", this.scrollTop > 160)
+	}
+	constructor(){
+		super()
+		this.addEventListener("scroll", e=>this.scroll(e))
+	}
 	render() {
 		return html`
 			<div class="content">
-				<img src="./img/me.jpg" class="me" alt="">
-				<div opacome class="name">
-					<h1 class="english">
-						Vagif Valiyev
-					</h1>
-					<h1 class="native">
-						Vaqif Vəliyev
-					</h1>
-					<div class="line"></div>
+				<div class="head">
+					<img src="./img/me.jpg" class="me" alt="">
+					<div opacome class="name">
+						<h1 class="english">
+							Vagif Valiyev
+						</h1>
+						<h1 class="native">
+							Vaqif Vəliyev
+						</h1>
+						<div class="line"></div>
+					</div>
+				</div>
+
+				<div class="about">
+					<div class="split2col">
+						<div class="mytree">
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+							fasad <br>fasad <br>fasad <br>fasad <br>fasad <br>
+						</div>
+						<div opacome class="shady-scroller texty">
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+							Education - UNEC <br>
+						</div>
+					</div>
 				</div>
 			</div>
 		`
