57e0b50260a07e708fc06ace6d814d168433362b
va9iff <va9iff@gmail.com>
2023-06-01 01:15:37 +0400

    view scrollbar bg transparent; projects gradient bg doesn't go over scrolls

--- src/views/projects/projects.js
+++ src/views/projects/projects.js
@@ -9,14 +9,10 @@ await new Promise(r=>setTimeout(r,500))
 class VProjects extends View {
 	static properties = {
 	}
-
 	render() {
 		return html`
-			<div class="bg" opacome></div>
 			<div opacome class="content">
-				${coolHTML("All my")} ${coolHTML("projects", {super: true})} ${coolHTML("will be shown here. If you don't want to wait, you can check my")}
-				${coolHTML("GitHub repos")} <a href="https://github.com/va9iff"><nobr>${coolHTML("@va9iff",{extra: true})}</nobr></a>.
-				${coolHTML("Vanyl, Ranture and Won projects will definitely be here. ")}
+				${coolHTML("All my ")}${coolHTML("projects", {super: true})}${coolHTML(` will be shown here. If you don't want to wait, you can check my GitHub repos `)}<a href="https://github.com/va9iff"><nobr>${coolHTML("@va9iff",{extra: true})}</nobr></a>${coolHTML(". Vanyl, Ranture and Won projects will definitely be here. ")}
 			</div>
 		`
 	}
