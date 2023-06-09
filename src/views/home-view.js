8c0d1eddafaa269c4888a0ac5c228ddea4528b22
va9iff <va9iff@gmail.com>
2023-06-10 02:52:29 +0400

    add metaballs

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,19 +1,42 @@
-import { html, VLitElement } from "../vlit.js"
+import { html, VLitElement, styleMap } from "../vlit.js"
 import {View} from "./view.js"
 
 class VHome extends View {
 	static properties = {
 		v9: { reflect: true },
+		x: {},
+		y: {}
 	}
 	constructor(){
 		super()
-		setTimeout(()=>this.setAttribute("v",parseInt(Math.random()*10)))
 	}
 	render() {
 		return html` 
 		<div class="content" fadeInOpacity>
-		<br>
-			Not one of your template dependant "developers"
+		<div class="punchline" exclusion>
+			<br>
+				Not one of your template dependant "developers"
+			</div>
+		</div>
+		<div class="metaeffect" @mousemove=${e=>{
+			this.x = e.clientX
+			this.y = e.clientY
+			this.requestUpdate()
+		}}>
+			<div class="m1"></div>
+			<div class="m2"></div>
+			<div class="m3" style=${styleMap({
+				left: this.x + "px",
+				top: this.y + "px"
+			})}></div>
+			<div class="m4"></div>
+			<div class="m5"></div>
+		</div>
+		<div class="metaballText" exclusion>
+			My implementation of Metaballs. <br>
+			Should perform better than Gaussian blur. <br>
+			Optimized, also more visually appealing.
+
 		</div>
 
 		`
