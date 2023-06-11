9ed2aa745873c032935a03d77465029cbb7dd388
va9iff <va9iff@gmail.com>
2023-06-12 02:25:21 +0400

    add cool cv closing animation like paper

--- src/views/cv/cv.js
+++ src/views/cv/cv.js
@@ -4,7 +4,7 @@ import {View} from "../view.js"
 class VCV extends View {
 	render() {
 		return html`
-		<div class="content" opacome>
+		<div class="content">
 			<div class="center">
 				<object data="https://va9iff.github.io/my/cv.pdf" type="application/pdf">
 				  <p>Unable to display PDF file. <a href="https://va9iff.github.io/my/cv.pdf">Download</a> instead.</p>
